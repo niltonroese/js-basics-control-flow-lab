09ee6ffb9414df53cec4ed8e95b5e0930428dbed
Nilton <niltonroese@gmail.com>
2022-03-02 15:51:50 +1000

    commit tree bc7d889d8d912c4923a0b5560f35793bfb6fb8fd

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(length) {
+  if (length <= 400) { 
+  return 'This one is on me!';
+} else if (length >2000 && length <2500) {
+  return 'I will gladly take your thirty bucks.';
+} else if (length >2500) {
+  return 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  const canDO = 'Ok, sounds good.';
+  const cannotDo = 'No go.';
+    return (city !== "NYC") ? cannotDo : canDO; 
+} 
+console.log(ternaryCheckCity("test"));
+
+function switchOnCharmFromTip(tip) {
+switch (tip) {
+case "generous":
+  return "Thank you so much.";
+  break;
+case "not as generous":
+  return "Thank you.";
+  break;
+default:
+  return "Bye.";
+  break;
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
