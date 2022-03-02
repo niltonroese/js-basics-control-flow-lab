5ce029a47fd536a31ee0e85eb898acf83f52338a
Nilton <niltonroese@gmail.com>
2022-03-02 15:53:18 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(length) {
+  if (length <= 400) { 
+  return 'This one is on me!';
+} else if (length >2000 && length <2500) {
+  return 'I will gladly take your thirty bucks.';
+} else if (length >2500) {
+  return 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  const canDO = 'Ok, sounds good.';
+  const cannotDo = 'No go.';
+    return (city !== "NYC") ? cannotDo : canDO; 
+} 
+console.log(ternaryCheckCity("test"));
+
+
+function switchOnCharmFromTip(tip) {
+switch (tip) {
+case "generous":
+  return "Thank you so much.";
+  break;
+case "not as generous":
+  return "Thank you.";
+  break;
+default:
+  return "Bye.";
+  break;
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  const statement1 = "Thank you so much.";
+  const statement2 = "Thank you.";
+  const statement3 = "Bye.";
+  if (tip === "generous") {
+    return statement1;
+  } else if (tip === "not as generous") {
+    return statement2;
+  } else if (tip === "thanks for everything") {
+    return statement3;
+  }
 }
